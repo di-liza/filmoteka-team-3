8233c55d769542225e04d111a82091b71cd6ef72
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-04-14 21:55:43 +0300

    fix loader and add new files for firebase

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import './js/movieCardModal';
 import './js/searchInput';
-import './sass/index.scss';
 // import './js/genres';
 // import './js/pagination'
 // import './js/apiFetch';
 // import './js/goTopBtn';
-// import './js/loader';
+import './js/loader';
 // import './js/pageTheme';
 // import './js/queueLocalStorage';
 // import './js/teamModal';
 // import './js/videoModal';
 // import './js/watchedLocalStorage';
 
+// Для добавления
+// import '';
+// import '';
+// import '';
+// import '';
+// import '';
+
+import './sass/index.scss';
