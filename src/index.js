33e80553635818a76c44117a162afd4cf1d5675f
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-04-14 21:28:56 +0300

    add imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import './js/renderCardMarkup';
-import './js/localStorage';
+// import './js/localStorage';
 import './js/movieCardModal';
 import './js/searchInput';
 import './sass/index.scss';
-
+// import './js/genres';
+// import './js/pagination'
+// import './js/apiFetch';
+// import './js/goTopBtn';
+// import './js/loader';
+// import './js/pageTheme';
+// import './js/queueLocalStorage';
+// import './js/teamModal';
+// import './js/videoModal';
+// import './js/watchedLocalStorage';
