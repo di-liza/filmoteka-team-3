638eba86dd159312467f6b50ff059146e31bdb0c
PilyaP <ppaexpert@gmail.com>
2023-04-15 02:14:19 +0300

    я сделал только модалка и пагинация остальное не могу...там надо стучать и добавлять к каждому асинхроному коду....єто надо делать по другому....
    я сделал только модалка и пагинация остальное не могу...там надо стучать и добавлять к каждому асинхроному коду....єто надо делать по другому.... надо добавлять refs.js с обьектами queryselector к класам и делать функцию, а не каждій отдельно...запуш мое...и я при поиске сломал тебе пагинацию...посмотри...ну у меня нет времени и не хочу подключать  refs.js, а то чтото сломаю....напиши мне в телегу...я могу все обьяснить....как бі работает не плохо...но не все...ну и то что я делаю єто геморой...

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,5 +1,79 @@
-export default window.onload = function () {
-    setTimeout(function () {
-        const overlay = document.querySelector('.overlay'); overlay.style.display = 'none';
-    }, 500);
-};
+const spinner = document.querySelector('.spinner');
+window.addEventListener('load', () => {
+  spinner.classList.add('hidden');
+});
+
+// Показываем спиннер при начале загрузки контента
+document.addEventListener('DOMContentLoaded', () => {
+  spinner.classList.remove('hidden');
+});
+
+// Скрываем спиннер при окончании загрузки контента
+window.addEventListener('DOMContentLoaded', () => {
+  spinner.classList.add('hidden');
+});
+
+// // на ссылки
+// const links = document.querySelectorAll('a');
+// for (const link of links) {
+//   link.addEventListener('click', () => {
+//     spinner.classList.remove('hidden');
+//   });
+// }
+
+// пагинациия
+const paginationLinks = document.querySelectorAll('#pagination');
+for (const link of paginationLinks) {
+  link.addEventListener('click', async (event) => {
+    event.preventDefault(); 
+    spinner.classList.remove('hidden');
+    const url = event.target.href;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Error Load');
+      }
+      const data = await response.json();
+      
+      updatePage(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      spinner.classList.add('hidden');
+    }
+  });
+}
+
+
+
+// модалка лоадер
+const modalLinks = document.querySelectorAll('.movie-collection');
+
+for (const link of modalLinks) {
+  link.addEventListener('click', async (event) => {
+    event.preventDefault();
+    spinner.classList.remove('hidden');
+    const url = event.target.href;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Error Load');
+      }
+      const data = await response.json();
+      updateModal(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      spinner.classList.add('hidden');
+    }
+  });
+}
+// поиск лоадер
+
+
+
+
+
+
+
+
