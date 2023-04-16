e4b5355cb6188df1e140aae3795ba40b613acf2a
Bilous-Oleh <2005net2005@gmail.com>
2023-04-16 23:42:45 +0300

    Footer

---
+++ src/js/sliderGlide.js
@@ -0,0 +1,10 @@
+// import Glide from '@glidejs/glide';
+
+// export const options = {
+//   type: 'carousel',
+//   startAt: 0,
+//   perView: 3,
+//   keyboard: true,
+// };
+
+// export const glideFooter = new Glide('.glide__footer', options);
