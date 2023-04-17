7bf057a8fe14533450210400d2e875eb42df1c03
bakmaiev <thisyoungrhyme@gmail.com>
2023-04-17 16:54:40 +0300

    Add dark-mode to team-modal

--- src/js/pageTheme.js
+++ src/js/pageTheme.js
@@ -1,12 +1,16 @@
 const checkbox = document.querySelector('.checkbox');
 const droplist = document.querySelector('.movie-droplist');
+const slider = document.querySelector('.glide__slides');
+console.log(slider);
 
 const handleCheckbox = () => {
   document.body.classList.toggle('dark');
   droplist.classList.toggle('dark');
+  slider.classList.toggle('dark');
   if (
     document.body.classList.contains('dark') &&
-    droplist.classList.contains('dark')
+    droplist.classList.contains('dark') &&
+    slider.classList.contains('dark')
   ) {
     localStorage.setItem('theme', 'dark');
   } else {
@@ -17,6 +21,7 @@ const handleCheckbox = () => {
 if (localStorage.getItem('theme')) {
   droplist.classList.toggle('dark');
   document.body.classList.toggle('dark');
+  slider.classList.toggle('dark');
   checkbox.checked = true;
 }
 
