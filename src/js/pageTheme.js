3b68d15d00b376fb29b5d5df829db796e14684a2
bakmaiev <thisyoungrhyme@gmail.com>
2023-04-17 19:06:40 +0300

    Add dark-mode to footer

--- src/js/pageTheme.js
+++ src/js/pageTheme.js
@@ -1,16 +1,14 @@
 const checkbox = document.querySelector('.checkbox');
 const droplist = document.querySelector('.movie-droplist');
 const slider = document.querySelector('.glide__slides');
+const footer = document.querySelector('.footer');
 
 const handleCheckbox = () => {
   document.body.classList.toggle('dark');
   droplist.classList.toggle('dark');
   slider.classList.toggle('dark');
-  if (
-    document.body.classList.contains('dark') &&
-    droplist.classList.contains('dark') &&
-    slider.classList.contains('dark')
-  ) {
+  footer.classList.toggle('dark');
+  if (document.body.classList.contains('dark')) {
     localStorage.setItem('theme', 'dark');
   } else {
     localStorage.removeItem('theme', 'dark');
@@ -18,9 +16,10 @@ const handleCheckbox = () => {
 };
 
 if (localStorage.getItem('theme')) {
-  droplist.classList.toggle('dark');
   document.body.classList.toggle('dark');
+  droplist.classList.toggle('dark');
   slider.classList.toggle('dark');
+  footer.classList.toggle('dark');
   checkbox.checked = true;
 }
 
