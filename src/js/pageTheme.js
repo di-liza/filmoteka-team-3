70182d37f7aff397e74e53d88744769c56a07de8
bakmaiev <thisyoungrhyme@gmail.com>
2023-04-18 14:54:26 +0300

    Fix team modal

--- src/js/pageTheme.js
+++ src/js/pageTheme.js
@@ -1,31 +1,39 @@
 const checkbox = document.querySelector('.checkbox');
 const droplist = document.querySelector('.movie-droplist');
+const slider = document.querySelector('.glide__slides');
+const footer = document.querySelector('.footer');
+const cross = document.querySelector('.team-member-modal__close-btn');
 import { LocalStorage } from './localStorage';
 
 const libraryLocalStorage = new LocalStorage();
-const slider = document.querySelector('.glide__slides');
-const footer = document.querySelector('.footer');
 
 const handleCheckbox = () => {
-  document.body.classList.toggle('dark');
-  droplist.classList.toggle('dark');
-  slider?.classList.toggle('dark');
-  footer.classList.toggle('dark');
+  turnToggle();
+
   if (document.body.classList.contains('dark')) {
     localStorage.setItem('theme', 'dark');
+    cross.style.cssText =
+      'stroke: var(--base-white); border-color: var(--base-white)';
     document.querySelector('.plug') && libraryLocalStorage.markupPlug();
   } else {
     localStorage.removeItem('theme', 'dark');
+    cross.style.cssText = 'stroke: inherit; border-color: inherit';
     document.querySelector('.plug') && libraryLocalStorage.markupPlug();
   }
 };
 
 if (localStorage.getItem('theme')) {
-  document.body.classList.toggle('dark');
-  droplist.classList.toggle('dark');
-  slider?.classList.toggle('dark');
-  footer.classList.toggle('dark');
+  turnToggle();
+  cross.style.cssText =
+    'stroke: var(--base-white); border-color: var(--base-white)';
   checkbox.checked = true;
 }
 
 checkbox.addEventListener('change', handleCheckbox);
+
+function turnToggle() {
+  document.body.classList.toggle('dark');
+  droplist.classList.toggle('dark');
+  slider?.classList.toggle('dark');
+  footer.classList.toggle('dark');
+}
